74c7d10f8b0d93df0ba7d4bfeea55fba127f5957
serhii2014 <92680438+serhii2014@users.noreply.github.com>
2021-11-02 13:54:46 +0200

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -33,7 +33,7 @@ function CountdownTimer(elm,tl,mes){
    function CDT(){
    
 	// Set countdown limit
-	var tl = new Date('2021/10/30 00:00:00');
+	var tl = new Date('2022/10/30 00:00:00');
    
 	// You can add time's up message here
 	var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">Время вышло!</span></span>');
